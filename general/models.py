2e0590447871bde3c3ca370828d3173f9a6b715a
Pgigurist <pgigurist@gmail.com>
2020-01-21 12:31:23 +0300

    add feedback & gallery urls & views

--- general/models.py
+++ general/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+class Photo(models.Model):
+    title = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='media/images/')
+    
+    class Meta:
+        verbose_name = 'фото'
+        verbose_name_plural = 'фотографии'
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    name = models.CharField(max_length=300)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=500, blank=True)
+    text = models.TextField(blank=True)
+    image = models.ForeignKey(Photo, on_delete=models.CASCADE, blank=True, default=None)
+    
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'новости'
+
+
+    def __str__(self):
+        return self.name
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    city = models.CharField(max_length=100)
+    text = models.TextField(blank=True)
+    moderate = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'отзыв'
+        verbose_name_plural = 'отзывы'
+
+
+    def __str__(self):
+        string = self.name
+        if self.moderate:
+            string += ' (опубликовано)'
+        else:
+            string += ' (новое)'
+        return string
+
---
+++ general/urls.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.conf.urls import url, include
+
+from . import views[
+    
+    url('gallery', views.gallery, name='gallery'),
+    ulr('feedback/', views.addFeedback, name='feedback'),
+]
