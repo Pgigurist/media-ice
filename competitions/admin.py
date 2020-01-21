6a14d885ad8998344b8ab78628c60b712143b921
Pgigurist <pgigurist@gmail.com>
2020-01-21 23:10:55 +0300

    add basic DB objects

--- competitions/admin.py
+++ competitions/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Event, Participant, Category, Entry 
 
 # Register your models here.
+
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('participant', 'category')
+
+class EntryInLine(admin.TabularInline):
+    model = Entry
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('event', 'name', 'date_start', 'date_end')
+    inlines = [EntryInLine] 
+
+admin.site.register(Event)
+admin.site.register(Participant)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Entry, EntryAdmin)
+
--- competitions/models.py
+++ competitions/models.py
@@ -2,3 +2,58 @@ from django.db import models
 
 # Create your models here.
 
+class Participant(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=80)
+    birthday = models.DateField()
+    club_name = models.CharField(max_length=110)
+    coach_name = models.CharField(max_length=100)
+
+    def full_name(self):
+        return self.first_name+' '+self.last_name
+
+    class Meta:
+        verbose_name = 'Участник'
+        verbose_name_plural = 'Участники'
+
+    def __str__(self):
+        return self.full_name()
+
+   
+class Event(models.Model):
+    name = models.CharField(max_length=150)
+    short_name = models.CharField(max_length=100)
+    abbreviation = models.CharField(max_length=10)
+    date_start = models.DateField()
+    date_end = models.DateField()
+
+    class Meta:
+        verbose_name = 'соревнование'
+        verbose_name_plural = 'соревнования'
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    date_start = models.DateTimeField()
+    date_end = models.DateTimeField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+
+class Entry(models.Model):
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Заявка'
+        verbose_name_plural = 'Заявки'
+
+
