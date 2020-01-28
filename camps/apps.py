179792f79a14c2ffdce350a3619ad27cb20954d3
Pgigurist <pgigurist@gmail.com>
2020-01-28 13:42:57 +0300

    add camps app and update models.py, admin.py

--- camps/apps.py
+++ camps/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-
 class CampsConfig(AppConfig):
     name = 'camps'
+    verbose_name = 'Сборы'
--- camps/models.py
+++ camps/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Camp(models.Model):
+    name = models.CharField(max_length=150)
+    place = models.CharField(max_length=200, blank=True)
+    short_name = models.CharField(max_length=100)
+    date_start = models.DateField()
+    date_end = models.DateField()
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'Лагерь'
+        verbose_name_plural = 'Сборы'
